c02127e49d5c30490437a9ba9f847aa839431a5f
Lenya Nab <nabiev.lenya@gmail.com>
2025-08-11 18:36:18 +0400

    Add migrations files

---
+++ migrations/001_init.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(128) NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
